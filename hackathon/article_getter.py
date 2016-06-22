60cbf70e25058978731ecb0d2d863c7c6d30535e
Joanna Misztal-Radecka <asiamsztl@gmail.com>
2016-06-22 10:56:56 +0200

    added recommenations

--- hackathon/article_getter.py
+++ hackathon/article_getter.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 import requests
 import json
 import lxml.html
 import unidecode
 from random import choice
+from models import InteractionType, UserArticleHistory
 
 
 class Parameters:
@@ -58,10 +60,11 @@ class ArticleImporter(object):
         query = ''
         if keywords:
             query = ' NOT '.join(keywords)  
-                             
-        different = self.__get_article(user=user, mode='search', params={'q':query, 'limit': limit})
-        
-        return self.__get_article_data(article)
+        category = article['category']
+        new_cat = choice(self.categories.keys())
+        different = self.__get_article(user=user, mode='search', params={'q':query, 'limit': limit, 'category': self.categories[new_cat]})
+
+        return self.__get_article_data(different['documents'][0])
     
     def get_next_article(self, user, article_id):
         article = self.__get_article(user=user, mode='search/{}'.format(article_id))
@@ -74,10 +77,20 @@ class ArticleImporter(object):
         return self.__get_article_data(new_article['documents'][0])
     
     def get_recommended_article(self, user):
-        articles = self.__get_article(user=user, mode='search',
-                                  params={'limit': 100, 'sortBy':'latest'})
+        history = UserArticleHistory.objects.exclude(user_id='1')        
         
-    
+        rec = Recommendation(history)
+        rec_article_id = rec.get_scores()
+        r = rec.get_user_recommendation(user, rec_article_id)
+#         return rec_article_id
+        article = self.__get_article(user=user, mode='search/{}'.format(r))
+#         return article
+#         return rec_article_id
+        
+        return self.__get_article_data(article)
+
+        
+
     def __process_text(self, text):
         return unidecode.unidecode(lxml.html.fromstring(text).text_content()).replace('\n','').replace("\'s",'')
     
@@ -91,6 +104,49 @@ class ArticleImporter(object):
                 })
         
     
+class Recommendation(object):
+    def __init__(self, history):
+        self.Q = self.get_Q(history)
+        self.lambda_ = 0.1
+        self.n_factors = 100
+        self.m, self.n = self.Q.shape
+        self.n_iterations = 20
+        
+    def get_Q(self, history):
+        interaction_mapping = {
+                               InteractionType.COMPLETED: 1,
+                               InteractionType.SKIPPED: -2,
+                               InteractionType.MORE: 2
+                               }
+        hist_data = pd.DataFrame(list(history.values('article_id', 'user_id','interaction').distinct()))
+        
+        hist_data['value'] = hist_data['interaction'].map(interaction_mapping)
+        hist_table = hist_data.pivot(index='user_id', columns='article_id', values='value')
+        
+        return hist_table.fillna(0)
+                     
+        
+    def get_scores(self):
+        X = np.random.rand(self.m, self.n_factors) 
+        Y = np.random.rand(self.n_factors, self.n)
+        errors = []
+        for ii in range(self.n_iterations):
+            X = np.linalg.solve(np.dot(Y, Y.T) + self.lambda_ * np.eye(self.n_factors), 
+                                np.dot(Y, self.Q.T)).T
+            Y = np.linalg.solve(np.dot(X.T, X) + self.lambda_ * np.eye(self.n_factors),
+                                np.dot(X.T, self.Q))
+            
+        Q_hat = np.dot(X, Y)
+        
+        return pd.DataFrame(Q_hat, index=self.Q.index, columns=self.Q.columns)
+    
+    def get_user_recommendation(self, user_id, scores):
+        return scores.loc[user_id].argmax()
+    
+        
+    def __get_error(self, Q, X, Y, W):
+        return np.sum((W * (Q - np.dot(X, Y)))**2)
+            
 class History(object):
     
     def __init__(self, history):
